3ea52a5ff46a3a1502067ee42f0d594b1439672c
Mike <kenr.mk@gmail.com>
2022-01-12 22:20:44 +0100

    webuath login cleanup

--- webauthn-skeleton/static/js/utils.js
+++ webauthn-skeleton/static/js/utils.js
@@ -1,56 +1,51 @@
-/* global base64 */
-/* exported preformatGetAssertReq, publicKeyCredentialToJSON, preformatMakeCredReq */
-
-/**
- * Converts PublicKeyCredential into serialised JSON
- * @param  {Object} pubKeyCred
- * @return {Object}            - JSON encoded publicKeyCredential
- */
-export const publicKeyCredentialToJSON = (pubKeyCred) => {
-	/* ----- DO NOT MODIFY THIS CODE ----- */
-	if (pubKeyCred instanceof Array) {
-		let arr = []
-		for (let i of pubKeyCred)
-			arr.push(publicKeyCredentialToJSON(i))
-
-		return arr
-	}
-
-	if (pubKeyCred instanceof ArrayBuffer) {
-		return base64.encode(pubKeyCred,true)
-	}
-
-	if (pubKeyCred instanceof Object) {
-		let obj = {}
-
-		for (let key in pubKeyCred) {
-			obj[key] = publicKeyCredentialToJSON(pubKeyCred[key])
-		}
-
-		return obj
-	}
-
-	return pubKeyCred
+export const publicKeyCredentialToJSON = cred => {
+	if (cred instanceof Array)       return reviveArrayPublicKey(cred)
+	if (cred instanceof ArrayBuffer) return reviveBufferPublicKey(cred)
+	if (cred instanceof Object)      return reviveObjectPublicKey(cred)
+	return cred
 }
 
-/**
- * Decodes arrayBuffer required fields.
- */
-export const preformatMakeCredReq = (makeCredReq) => {
-	makeCredReq.challenge = base64.decode(makeCredReq.challenge,true)
-	makeCredReq.user.id = base64.decode(makeCredReq.user.id,true)
+function reviveArrayPublicKey(cred) {
+	//console.log('array() ---')
+	let arr = []
+	for (let i of cred)
+		arr.push(publicKeyCredentialToJSON(i))
 
-	// Decode id of each excludeCredentials
-	if (makeCredReq.excludeCredentials) {
-		makeCredReq.excludeCredentials = makeCredReq.excludeCredentials.map((e) => { return { id: base64.decode(e.id, true), type: e.type }})
+	return arr
+}
+
+function reviveBufferPublicKey(cred) {
+	//console.log('buffer() ---')
+	return uint8ToBase64(cred)
+}
+
+function reviveObjectPublicKey(input) {
+	//console.log('object() -------------------------------')
+    //console.log('input', input)
+    //console.log('Object.keys(input)', Object.keys(input))
+	let output = {}
+	for (let key in input) {
+		output[key] = publicKeyCredentialToJSON(input[key])
+		//console.log('>', key, input[key], output[key])
 	}
+    //console.log('output', output)
+    //console.log('Object.keys(output)', Object.keys(output))
+	return output
+}
 
+export const preformatMakeCredReq = (makeCredReq) => {
+	makeCredReq.challenge = base64ToUint8(makeCredReq.challenge)
+	makeCredReq.user.id = base64ToUint8(makeCredReq.user.id)
+	if (makeCredReq.excludeCredentials)
+		makeCredReq.excludeCredentials = makeCredReq.excludeCredentials.map(reviveCred)
 	return makeCredReq
 }
 
-/**
- * Decodes arrayBuffer required fields.
- */
+const reviveCred = cred => ({
+	id: base64ToUint8(cred.id),
+	type: cred.type
+})
+
 export const preformatGetAssertReq = getAssert => {
 	getAssert.challenge = base64ToUint8(getAssert.challenge)
 	for (let cred of getAssert.allowCredentials)
--- webauthn-skeleton/static/js/webauthn.auth.js
+++ webauthn-skeleton/static/js/webauthn.auth.js
@@ -1,54 +1,32 @@
 import {publicKeyCredentialToJSON, preformatMakeCredReq, preformatGetAssertReq, postJson} from './utils.js'
 import {loadMainContainer} from './view.js'
 
-let getMakeCredentialsChallenge = (formBody, additional) => {
-	return postJson(additional ? '/webauthn/add' : '/webauthn/register', formBody)
+let getMakeCredentialsChallenge = async body => {
+	let response = await postJson('/webauthn/register', body)
+	return preformatMakeCredReq(response)
 }
 
-let sendWebAuthnResponse = (body) => {
-	return postJson('/webauthn/response', body)
-}
-
-let getGetAssertionChallenge = async (formBody) => {
-	let response = await postJson('/webauthn/login', formBody)
+let getGetAssertionChallenge = async body => {
+	let response = await postJson('/webauthn/login', body)
 	return preformatGetAssertReq(response)
 }
 
-/* Handle for register form submission */
-export async function register(username, additional) {
-	let name = username
-	try {
-		let response1 = await getMakeCredentialsChallenge({username, name}, additional)
-        console.log('~ response1', response1)
-		let publicKey = preformatMakeCredReq(response1)
-		let response2 = await navigator.credentials.create({ publicKey })
-        console.log('~ response2.response.attestationObject', response2.response.attestationObject)
-		let makeCredResponse = {
-			id: response2.id,
-			rawId: base64.encode(response2.rawId,true),
-			response: {
-				attestationObject: base64.encode(response2.response.attestationObject,true),
-				clientDataJSON: base64.encode(response2.response.clientDataJSON,true)
-			},
-			type: response2.type
-		}
-		await sendWebAuthnResponse(makeCredResponse)
-		loadMainContainer()   
-	} catch(error) {
-		alert(error)
-	}
+let sendWebAuthnResponse = raw => {
+	let json = publicKeyCredentialToJSON(raw)
+	return postJson('/webauthn/response', json)
 }
 
-/* Handler for login form submission */
+export async function register(username) {
+	let name = username
+	let publicKey = await getMakeCredentialsChallenge({username, name})
+	let credential = await navigator.credentials.create({publicKey})
+	await sendWebAuthnResponse(credential)
+	loadMainContainer()
+}
+
 export async function login() {
-	try {
-		let publicKey = await getGetAssertionChallenge()
-        console.log('~ publicKey', publicKey)
-		let response2 = await navigator.credentials.get({publicKey})
-		let getAssertionResponse = publicKeyCredentialToJSON(response2)
-		await sendWebAuthnResponse(getAssertionResponse)
-		loadMainContainer()   
-	} catch(error) {
-		alert(error)
-	}
+	let publicKey = await getGetAssertionChallenge()
+	let credential = await navigator.credentials.get({publicKey})
+	await sendWebAuthnResponse(credential)
+	loadMainContainer()
 }

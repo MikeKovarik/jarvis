107357f264dfdc4441ee9dbf133ff47625200d94
Mike <kenr.mk@gmail.com>
2022-01-22 01:43:31 +0100

    removed ssr

--- webauthn-codelab-start/server.js
+++ webauthn-codelab-start/server.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import session from 'express-session'
-import hbs from 'hbs'
 import auth from './routes/auth.js'
 const app = express()
 
@@ -8,9 +7,6 @@ const app = express()
 const port = 8080
 const secret = 'TODO'
 
-app.set('view engine', 'html')
-app.engine('html', hbs.__express)
-app.set('views', './views')
 app.use(express.json())
 app.use(express.static('public'))
 
@@ -24,10 +20,6 @@ app.use(session({
 }))
 
 app.use('/auth', auth)
-
-app.get('/', (req, res) => {
-	res.render('index.html')
-})
 
 const listener = app.listen(port, () => {
 	console.log('Your app is listening on port ' + listener.address().port)

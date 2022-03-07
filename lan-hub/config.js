ccb84fed1d0fe5df0ac0328830c07236d324975a
Mike <kenr.mk@gmail.com>
2022-03-07 21:52:33 +0100

    ble experimentation

--- lan-hub/config.js
+++ lan-hub/config.js
@@ -2,10 +2,14 @@ import dotenv from 'dotenv'
 import {getAbsolutePath, readJson, readYaml} from './util/util.js'
 
 
-dotenv.config()
+let envPath = getAbsolutePath(import.meta.url, '../.env')
+const [,,envType] = process.argv
+if (envType) envPath += `.${envType}`
+
+dotenv.config({path: envPath})
 
 const dataPath      = getAbsolutePath(import.meta.url, '../data')
-const configPath    = getAbsolutePath(import.meta.url, '../data/config.json')
+const configPath    = getAbsolutePath(import.meta.url, `../data/${['config', envType].filter(a => a).join('-')}.json`)
 const z2mConfigPath = getAbsolutePath(import.meta.url, '../data/configuration.yaml')
 
 process.env.ZIGBEE2MQTT_DATA = dataPath

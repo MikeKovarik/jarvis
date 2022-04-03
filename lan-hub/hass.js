a0e6fbcb2f7b24f6c87da6cf04b9ede51d115cf7
Mike <kenr.mk@gmail.com>
2022-04-03 21:59:19 +0200

    fixed import

--- lan-hub/hass.js
+++ lan-hub/hass.js
@@ -1,6 +1,6 @@
 import {spawn} from 'child_process'
 import {exposeThroughProxy} from 'lan-tunnel'
-import config from '../config.js'
+import config from './config.js'
 
 
 const process = spawn('hass', [], {stdio: 'inherit'})

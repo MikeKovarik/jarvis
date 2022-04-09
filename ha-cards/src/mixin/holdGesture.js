b1d9a94c2475da6e9ca832cf6d3173de950ba6e2
Mike <kenr.mk@gmail.com>
2022-04-09 21:12:18 +0200

    changed way hold gesture calculates movement

--- ha-cards/src/mixin/holdGesture.js
+++ ha-cards/src/mixin/holdGesture.js
@@ -1,5 +1,9 @@
 import {tapHoldThreshold, tapDragThreshold} from '../util/const.js'
 
+
+// We somewhat adjust the constant to account for additive way of calculating move distance we're doing here.
+const movementThreshold = tapDragThreshold * 1.5
+
 // WARNING: contextmenu behaves differently on phones than on desktop (longpress touch)
 // desktop: fires after pointerup
 // phone:   fires while still hodlding (ie. before pointerup). after about 500-700ms
@@ -8,9 +12,10 @@ export const holdGesture = Base => class extends Base {
 
 	#timeout = undefined
 	#gestureActive = false
-	#initEvent = undefined
 	#lastEvent = undefined
 
+	#movementX = 0
+	#movementY = 0
 
 	connectedCallback() {
 		this.addEventListener('pointerdown', this.#onPointerDown)
@@ -36,33 +41,38 @@ export const holdGesture = Base => class extends Base {
 		document.removeEventListener('pointercancel', this.#onPointerUp, {capture: true})
 	}
 
-	#onPointerDown = e => {
-		this.#gestureActive = false
-		clearTimeout(this.#timeout)
-		this.#addAditionalEvents()
-		this.#timeout = setTimeout(this.#onTimeout, tapHoldThreshold)
-		this.#initEvent = this.#lastEvent = e
-	}
-
-	#onPointerMove = e => {
-		this.#lastEvent = e
-		if (this.#gestureActive) e.preventDefault()
-	}
-
-	#onTimeout = () => {
-		if (isWithinThreshold(this.#initEvent, this.#lastEvent, tapDragThreshold)) {
-			const {x, y} = this.#lastEvent
-			this.emit('hold', {x, y})
-			this.#gestureActive = true
-		}
-		this.#clearTimeout()
-	}
-
 	#onContextMenu = e => {
 		// WARNING: do not pair with #onPointerUp nor try to use with #gestureActive.
 		// The event may fire 1) before, 2) after 3) never (if slightly moved) and is unreliable.
 		// We can only safely limit the prevention to this element.
 		if (e.target === this) e.preventDefault()
+	}
+
+	#onPointerDown = e => {
+		this.#gestureActive = false
+		clearTimeout(this.#timeout)
+		this.#addAditionalEvents()
+		this.#timeout = setTimeout(this.#onTimeout, tapHoldThreshold)
+		this.#lastEvent = e
+	}
+
+	#onPointerMove = e => {
+		if (this.#gestureActive) {
+			e.preventDefault()
+		} else {
+			this.#movementX += Math.abs(e.movementX)
+			this.#movementY += Math.abs(e.movementY)
+			this.#lastEvent = e
+		}
+	}
+
+	#onTimeout = () => {
+		if (this.#movementX < movementThreshold && this.#movementY < movementThreshold) {
+			const {x, y} = this.#lastEvent
+			this.emit('hold', {x, y})
+			this.#gestureActive = true
+		}
+		this.#clearTimeout()
 	}
 
 	#onPointerUp = e => {
@@ -82,7 +92,8 @@ export const holdGesture = Base => class extends Base {
 	#reset = e => {
 		this.#gestureActive = false
 		this.#lastEvent = undefined
-		this.#initEvent = undefined
+		this.#movementX = 0
+		this.#movementY = 0
 		this.#removeAditionalEvents()
 	}
 

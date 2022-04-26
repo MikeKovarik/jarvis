7b865070081918e0d012690e1ec1e2081c1ea7d1
Mike <kenr.mk@gmail.com>
2022-04-26 10:09:58 +0200

    wip

--- ha-cards/src/card/flower.js
+++ ha-cards/src/card/flower.js
@@ -29,54 +29,63 @@ class FlowerCard extends slickElement(hassData) {
 			user-select: none;
 		}
 
-		:host,
-		:host([size="medium"]) {
+		:host([size="small"]) {
 			--flower-height: 240px;
-			--flower-header-size: 1.5rem;
+			--flower-header-size: 1.125rem;
 		}
 
-		:host([size="small"]) {
-			--flower-height: 160px;
+		:host,
+		:host([size="medium"]) {
+			--flower-height: 280px;
 			--flower-header-size: 1.25rem;
 		}
 
-		h2 {
-			font-weight: 400;
-			font-size: var(--flower-header-size);
+		:host([size="large"]) {
+			--flower-height: 360px;
+			--flower-header-size: 1.5rem;
 		}
 
-		#image {
+		ha-card {
 			background-size: cover;
 			background-position: center;
-			width: 100%;
 			height: var(--flower-height);
 			position: relative;
 		}
-			#image h2 {
+
+			#wrapper {
 				position: absolute;
-				bottom: 0;
-				left: 0;
 				right: 0;
-				padding: 1rem;
-				margin: 0;
-				display: block;
-				z-index: 0;
+				left: 0;
+				bottom: 0;
+				z-index: 1;
 			}
-			#image h2::after {
+			#wrapper::after {
 				content: '';
 				position: absolute;
 				inset: 0;
 				background-color: var(--card-background-color);
-				opacity: 0.8;
+				opacity: 0.9;
 				z-index: -1;
 			}
 
-		#grid {
-			display: grid;
-			padding: 1rem;
-			gap: 0.5rem 2rem;
-			grid-template-columns: repeat(2, 1fr);
-		}
+			h2 {
+				margin: 0;
+				padding: 1.25rem 1rem 0rem 1rem;
+				display: block;
+				text-transform: capitalize;
+				font-weight: 400;
+				font-size: var(--flower-header-size);
+				white-space: nowrap;
+				overflow: hidden;
+				text-overflow: ellipsis;
+			}
+
+			#grid {
+				display: grid;
+				padding: 1rem;
+				gap: 0.5rem 2rem;
+				grid-template-columns: repeat(2, 1fr);
+			}
 	`
 
 	getImagePath(name, root) {
@@ -108,15 +117,15 @@ class FlowerCard extends slickElement(hassData) {
 		//entity.friendly_name: "plant_c47c8d6da8d7"
 
 		return html`
-			<ha-card>
-				<div id="image" style="background-image: ${this.backgroundImage}">
+			<ha-card style="background-image: ${this.backgroundImage}">
+				<div id="wrapper">
 					<h2>${title}</h2>
-				</div>
-				<div id="grid">
-					<slick-icon-chart title="${attrs?.brightness} ${units.brightness}"     icon="mdi:thermometer-low" value="100"></slick-icon-chart>
-					<slick-icon-chart title="${attrs?.conductivity} ${units.conductivity}" icon="hass:white-balance-sunny" value="75"></slick-icon-chart>
-					<slick-icon-chart title="${attrs?.moisture} ${units.moisture}"         icon="mdi:watering-can-outline" value="50"></slick-icon-chart>
-					<slick-icon-chart title="${attrs?.temperature} ${units.temperature}"   icon="mdi:emoticon-poop" value="25"></slick-icon-chart>
+					<div id="grid">
+						<slick-icon-chart title="${attrs?.brightness} ${units.brightness}"     icon="mdi:thermometer-low" value="100"></slick-icon-chart>
+						<slick-icon-chart title="${attrs?.conductivity} ${units.conductivity}" icon="hass:white-balance-sunny" value="75"></slick-icon-chart>
+						<slick-icon-chart title="${attrs?.moisture} ${units.moisture}"         icon="mdi:water-outline" value="50"></slick-icon-chart>
+						<slick-icon-chart title="${attrs?.temperature} ${units.temperature}"   icon="mdi:emoticon-poop" value="25"></slick-icon-chart>
+					</div>
 				</div>
 			</ha-card>
 		`
@@ -152,6 +161,19 @@ class IconChart extends slickElement() {
 
 	static styles = css`
 		:host {
+			--color: rgb(128, 128, 128);
+		}
+		:host([color="red"]) {
+			--color: rgb(255, 48, 58);
+		}
+		:host([color="orange"]) {
+			--color: rgb(175, 120, 0);
+		}
+		:host([color="green"]) {
+			--color: rgb(105, 200, 10);
+		}
+
+		:host {
 			display: flex;
 			align-items: center;
 		}
@@ -169,12 +191,12 @@ class IconChart extends slickElement() {
 				.indicator {
 					width: 0%;
 					height: 100%;
-					background-color: rgba(128, 128, 128, 0.5);
+					background-color: var(--color);
+					opacity: 0.5;
 				}
 	`
 
 	render() {
-            console.log('~ this.icon', this.icon, this.value)
 		return html`
 			<ha-icon icon="${this.icon}"></ha-icon>
 			<div class="bar" id="temp">

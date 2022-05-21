05c75fc93817fb1e2f0f4a2014e608b434cb51e3
Mike <kenr.mk@gmail.com>
2022-05-21 13:36:05 +0200

    removed hass proxy from hass file

--- lan-hub/hass.js
+++ lan-hub/hass.js
@@ -7,9 +7,10 @@ const process = spawn('hass', [], {stdio: 'inherit'})
 
 process.once('close', code => process.exit(code))
 process.once('exit', code => process.exit(code))
-
+/*
 exposeThroughProxy({
 	log: true,
 	...config,
 	...config['proxy-hass'],
 })
+*/
